49744ec2ca7bc0915fb4f79fbb615f4e37035cca
Cnamm7 <Cna.mm7@gmail.com>
2023-11-20 12:28:01 -0500

    completed DesignPDFViewer challenge

--- DataStructures/src/main/java/Main/test.java
+++ DataStructures/src/main/java/Main/test.java
@@ -2015,6 +2015,29 @@ public class test {
         }
 
     }
+
+    class ResultDesignPDFViewer {
+
+        /*
+         * Complete the 'designerPdfViewer' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY h
+         *  2. STRING word
+         */
+
+        public static int designerPdfViewer(List<Integer> h, String word) {
+            // Write your code here
+            int max = 0;
+            for (int i = 0; i < word.length(); i++) {
+                int index = word.charAt(i) - 97;
+                max = Math.max(max, h.get(index));
+            }
+            return max * word.length();
+        }
+
+    }
 }
 
 
