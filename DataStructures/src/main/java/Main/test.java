a146438fd09690ce21eb2020a7557c10b6bbad0f
Cnamm7 <Cna.mm7@gmail.com>
2023-11-20 19:20:13 -0500

    Completing Angry Professor challenge

--- DataStructures/src/main/java/Main/test.java
+++ DataStructures/src/main/java/Main/test.java
@@ -2062,6 +2062,27 @@ public class test {
         }
 
     }
+
+    class ResultAngryProfessor {
+
+        /*
+         * Complete the 'angryProfessor' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts following parameters:
+         *  1. INTEGER k
+         *  2. INTEGER_ARRAY a
+         */
+
+        public static String angryProfessor(int k, List<Integer> a) {
+            // Write your code here
+            int onTime = 0;
+            for (int student : a) {
+                if (student <= 0) onTime++;
+            }
+            return onTime >= k ? "NO" : "YES";
+        }
+    }
 }
 
 
