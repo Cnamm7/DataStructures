909d6630418595a8a8dca0629ff2e96dbf97e189
Cnamm7 <Cna.mm7@gmail.com>
2023-11-25 17:14:50 -0500

    Completed commenting the Sort interface and SortImpl

--- DataStructures/src/main/java/dataStructures/Sort.java
+++ DataStructures/src/main/java/dataStructures/Sort.java
@@ -1,9 +1,47 @@
 package dataStructures;
-
+/**
+ * implementing array sorting different methods
+ */
 public interface Sort {
+    /**
+     * basic sorting algorithm, starting by moving the greatest value to the end, and do the same for other elements
+     * time complexity = O (n ^ 2)
+     * @param arr
+     */
     public void bubbleSort(int[] arr);
+
+    /**
+     * start from the index zero and go through the length of array, will keep the record of the index of lower value
+     * and then swap it with current index, in this case our list will start to sort from right to left low to high
+     * time complexity = O (n ^ 2)
+     * @param arr
+     */
     public void selectionSort(int[] arr);
+
+    /**
+     * this sort starts from beginning and continue sorting by comparing each element by previous ones, unless putting each element
+     * at the right place
+     * time complexity = O (n ^ 2)
+     * @param arr
+     */
     public void insertionSort(int[] arr);
+
+    /**
+     * Breaking the array into half until it has one element which is sorted by default,
+     * then it starts to merge arrays to finally create a sorted array
+     * time complexity = O (n * log(n))
+     * @param arr
+     * @return
+     */
     public int[] mergeSort(int[] arr);
+
+    /**
+     * in quick sort we are going to get a pivot, starting from the first index, go through the array and sort all the items according
+     * to the pivot until the end, and at the end of itteration, all the items in the array are either less than pivot to left side
+     * and or more than pivot to the right sid, then we change the pivot with the last item to put pivot to the correct place, this
+     * action will continue recursively until we reach a point that we have a sorted array
+     * time complexity = O (n * log(n))
+     * @param arr
+     */
     public void quickSort(int[] arr);
 }
--- DataStructures/src/main/java/dataStructuresImpl/SortImpl.java
+++ DataStructures/src/main/java/dataStructuresImpl/SortImpl.java
@@ -4,10 +4,15 @@ import dataStructures.Sort;
 
 import java.util.Arrays;
 
+/**
+ * implementing different sort strategies, general information for each sort is provided in interface class
+ */
 public class SortImpl implements Sort {
 
     @Override
     public void bubbleSort(int[] arr) {
+        // starting from the last element, and move all item from the start to the top if their value is greater than the
+        // previous one
         for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -21,7 +26,7 @@ public class SortImpl implements Sort {
 
     @Override
     public void selectionSort(int[] arr) {
-
+        // starting by the lowest index and keeping track of the lowest value index
         for (int i = 0; i < arr.length; i++) {
             int j = i + 1;
             int minIndex = i;
@@ -31,6 +36,8 @@ public class SortImpl implements Sort {
                 }
                 j++;
             }
+            // if it's not equal, will swap the value, so we have low at the right while we go through
+            // the list
             if (minIndex != i) {
                 int temp = arr[i];
                 arr[i] = arr[minIndex];
@@ -41,6 +48,8 @@ public class SortImpl implements Sort {
 
     @Override
     public void insertionSort(int[] arr) {
+        // starting from start, go util the end and for each element while it is less than the previous one, move it unless
+        // it places at the right order
         for (int i = 0; i < arr.length; i++) {
             int k = i;
             for (int j = i - 1; j > -1; j--) {
@@ -56,22 +65,33 @@ public class SortImpl implements Sort {
 
     @Override
     public int[] mergeSort(int[] arr) {
+        // if it was 1, then it will be our breaking point, return the array
         if (arr.length == 1) return arr;
 
         int midIndex = arr.length / 2;
 
+        // passing the half of arrays, one left and one right
         int[] left = mergeSort(Arrays.copyOfRange(arr, 0, midIndex));
         int[] right = mergeSort(Arrays.copyOfRange(arr, midIndex, arr.length));
 
+        // finally, merge all arrays through the call stack to build sorted array
         return merge(left, right);
     }
 
+    /**
+     * merge function is sub function for merge sort, it merges two passed array in sorted way
+     * @param a
+     * @param b
+     * @return
+     */
     private int[] merge(int[] a, int[] b) {
+        // create an array with length of two array we want to merge
         int[] combined = new int[a.length + b.length];
         int index = 0;
         int i = 0;
         int j =0;
 
+        // start to add items to merge array until it reaches to the end of one of the arrays
         while (i < a.length && j < b.length) {
             if (a[i] < b[j]) {
                 combined[index] = a[i];
@@ -84,12 +104,13 @@ public class SortImpl implements Sort {
             }
         }
 
+        // here if first array still has items and second one is empty, simply add the remaining items
         while (i < a.length){
             combined[index] = a[i];
             index++;
             i++;
         }
-
+        // here if second array still has items and first one is empty, simply add the remaining items
         while (j < b.length){
             combined[index] = b[j];
             index++;
@@ -100,18 +121,40 @@ public class SortImpl implements Sort {
 
     @Override
     public void quickSort(int[] arr) {
+        // start the quick sort recursively by setting the left index from 0 and right index to last index
         quickSort(arr, 0, arr.length - 1);
     }
 
+    /**
+     * passing the left and right to quickSort to complete function recursively
+     * @param arr
+     * @param left
+     * @param right
+     */
     private void quickSort(int[] arr, int left, int right) {
+        // check whether left is less than right, to see if left and right reached or not
         if (left < right) {
+            // sort based on the pivot and return the pivot
             int swapIndex = pivot(arr, left, right);
+            // call quickSort recursively from left to before swap index to completely sort the array
             quickSort(arr, left, swapIndex - 1);
+            // call quickSort recursively from after swap index to right to completely sort the array
             quickSort(arr, swapIndex + 1, right);
         }
     }
+
+    /**
+     * pivot function takes pivot index and the index that it should sort the array to
+     * @param arr
+     * @param pivotIndex
+     * @param endIndex
+     * @return
+     */
     private int pivot(int[] arr, int pivotIndex, int endIndex) {
+        // set swapIndex
         int swapIndex = pivotIndex;
+        // go from after pivot to the end and swap whenever an element that swapIndex is on is less than the pivot, swap it with
+        // swapIndex and move swapIndex to one forward and continue to iterate in the list
         for (int i = pivotIndex + 1; i <= endIndex; i++) {
             if (arr[i] < arr[pivotIndex]) {
                 swapIndex++;
@@ -123,6 +166,12 @@ public class SortImpl implements Sort {
         return swapIndex;
     }
 
+    /**
+     * swap is helper function to simply swap array elements
+     * @param arr
+     * @param swapIndex
+     * @param i
+     */
     private void swap(int[] arr, int swapIndex, int i) {
         int temp = arr[swapIndex];
         arr[swapIndex] = arr[i];
